d769e37c70272a88b987b5d140b32b72bcd49bca
nestorvave <84485292+nestorvave@users.noreply.github.com>
2025-10-02 23:26:05 -0600

    Initial project setup: Spring Boot with PostgreSQL Docker container and basic configuration

---
+++ src/main/java/com/accio/point_sale/PointSaleApplication.java
@@ -0,0 +1,13 @@
+package com.accio.point_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointSaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accio/point_sale/PointSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accio.point_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
