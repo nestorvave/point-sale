4b89ae1dea60d383726c61a0ba4d66ef53c7289b
nestorvave <84485292+nestorvave@users.noreply.github.com>
2025-10-19 02:02:00 -0600

    FTR:Add Sales swagger description

--- src/main/java/com/accio/point_sale/controllers/SaleController.java
+++ src/main/java/com/accio/point_sale/controllers/SaleController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accio.point_sale.domain.dtos.Sale.SaleCreateDtoRequest;
 import com.accio.point_sale.domain.dtos.Sale.SaleDtoResponse;
+import com.accio.point_sale.domain.dtos.User.UserDtoResponse;
 import com.accio.point_sale.domain.entities.Sale;
 import com.accio.point_sale.domain.entities.User;
 import com.accio.point_sale.mappers.SaleMapper;
 import com.accio.point_sale.services.Sale.SaleService;
 import com.accio.point_sale.services.User.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -29,6 +35,11 @@ public class SaleController {
 	private final UserService userService;
 	private final SaleMapper saleMapper;
 
+	@Operation(summary = "Get all sales", description = "Retrieve a list of all sales")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "List of sales retrieved successfully", content = @Content(schema = @Schema(implementation = SaleDtoResponse.class))),
+			@ApiResponse(responseCode = "404", description = "No sales found", content = @Content(schema = @Schema()))
+	})
 	@GetMapping()
 	public ResponseEntity<List<SaleDtoResponse>> getAllSales() {
 		List<Sale> sales = saleService.getAllSales();
@@ -36,6 +47,11 @@ public class SaleController {
 		return ResponseEntity.ok(sales.stream().map(saleMapper::toDtoResponse).toList());
 	}
 
+	@Operation(summary = "Create a new sale", description = "Add a new sale to the system")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sale created successfully", content = @Content(schema = @Schema(implementation = SaleDtoResponse.class))),
+			@ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content(schema = @Schema()))
+	})
 	@PostMapping
 	public ResponseEntity<SaleDtoResponse> create(@RequestBody SaleCreateDtoRequest saleRequest) {
 		User user = userService.findById(saleRequest.getUserID());
